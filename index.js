9a42b13612382332cbc5c767a18040436fa12836
Lasse Bramer Schmidt <lassebramer@gmail.com>
2022-12-05 12:20:33 +0100

    updated DB and should be ready to run

--- index.js
+++ index.js
@@ -29,9 +29,6 @@ app.get("/playerProps", (req, res) => {
           throw err;
         }
         res.send(rows)
-        rows.forEach((row) => {
-          console.log(row);
-        });
       });
 })
 
@@ -46,13 +43,60 @@ app.post("/getPlayerPropsByDate", (req, res) => {
               throw err;
             }
             res.send(rows)
-            rows.forEach((row) => {
-              console.log(row);
-            });
           });
     }
-  })
+})
+
+app.post("/getPlayerId", (req, res) => {
+if (!req.body.name) {
+    res.json("No Name found in reqest body.")
+} else {
+    sql = 'SELECT PlayerID from Players where Name = ?'
+    playerName = req.body.name
+    db.get(sql, playerName, (err, row) => {
+        if (err) {
+            throw err;
+        }
+        res.send(row["PlayerID"])
+        });
+}
+})
+
+app.post("/getPlayerStatsBySeasonAndID", (req, res) => {
+  console.log(req.body)
+    if (!req.body.season) {
+        res.json("No Season found in reqest body.")
+    } else if(!req.body.id){
+        res.json("No PlayerID found in reqest body.")
+    } else {
+        sql = 'SELECT *  from boxScores Where Season = ? and PlayerId = ?'
+        season = req.body.season
+        id = req.body.id
+        db.all(sql, [season,id], (err, rows) => {
+            if (err) {
+                throw err;
+            }
+            res.send(rows)
+            });
+    }
+})
+
+app.post("/insertPlayerProps", (req, res) => {
+    if (!req.body) {
+        res.json("No Season found in reqest body.")
+    } else {
+        stats = req.body
+        db.run(`INSERT OR REPLACE INTO playerProps (Game,Season, PLayer, Date, PL, PO, PU, PLL, PLO, PLU, PHL, PHO, PHU, RL, RO, RU, AL, AO, AU, threePL, threePO, threePU, SL, SO, SU, TL, Tover, TU, BL, BO, BU, DDO, DDU, TDO, TDU, PAL, PAO, PAU, PRL, PRO, PRU, PRAL, PRAO, PRAU, ARL, ARO, ARU, SBL, SBO, SBU) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`, stats, function(err) {
+          if (err) {
+            res.json(err.message);
+          }else{
+            res.json("success")
+          }
+        });
+    }
+})
 
 app.listen(PORT, function () {
   console.log(`Express server listening on port ${PORT}`)
 })
+
